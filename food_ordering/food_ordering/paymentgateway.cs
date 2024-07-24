be66ca8716d65a02f214fb4e918b2fd0b1e2c1ee
Udara Wickramarathne <152800175+UdaraWickramarathne@users.noreply.github.com>
2024-07-24 16:08:21 +0530

    Remove the link.cs

--- food_ordering/food_ordering/paymentgateway.cs
+++ food_ordering/food_ordering/paymentgateway.cs
@@ -119,7 +119,7 @@ namespace food_ordering
                 //generateQR
                 try
                 {
-                    string url = $"http://my-qr-code-website.s3-website.eu-north-1.amazonaws.com?id={id}";
+                    string url = $"example.com}";
 
                     QRCodeGenerator qrGenerator = new QRCodeGenerator();
                     QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
